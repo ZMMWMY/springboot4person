ac430416dbdd5e93f7428074cc7cd88efdcccf96
ZMMWMY <13758212259@163.com>
2016-10-27 17:12:00 +0800

    websocket

---
+++ boot-websocket/src/main/java/com/SocketStart.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Z先生 on 2016/10/27.
+ */
+@SpringBootApplication
+public class SocketStart {
+    public static void main(String[] args) {
+        SpringApplication.run(SocketStart.class, args);
+    }
+}
