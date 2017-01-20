bc479dcca8a58e9ebcaf0cf5a548ca49b29b558a
ZMMWMY <13758212259@163.com>
2017-01-20 15:12:18 +0800

    fastdfs

---
+++ boot-mybatis-again/src/main/java/com/mrz/mybatis/again/Start.java
@@ -0,0 +1,14 @@
+package com.mrz.mybatis.again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Z先生 on 2017/1/20.
+ */
+@SpringBootApplication
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+}
