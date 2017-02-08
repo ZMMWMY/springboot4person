3423c225fe30c8f356d440d32e9305a2ff2d862c
ZMMWMY <13758212259@163.com>
2017-02-08 17:19:57 +0800

    webmagic

---
+++ boot-webmagic/src/main/java/com/boot/webmagic/MagicStart.java
@@ -0,0 +1,14 @@
+package com.boot.webmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Z先生 on 2017/2/8.
+ */
+@SpringBootApplication
+public class MagicStart {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicStart.class, args);
+    }
+}
