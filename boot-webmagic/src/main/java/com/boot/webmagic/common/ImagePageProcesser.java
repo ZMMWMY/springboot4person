3b13881018b89902029e6f6aaca63e3423b3a73d
ZMMWMY <13758212259@163.com>
2017-02-08 17:34:34 +0800

    fastdfs

---
+++ boot-webmagic/src/main/java/com/boot/webmagic/common/ImagePageProcesser.java
@@ -0,0 +1,20 @@
+package com.boot.webmagic.common;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by Z先生 on 2017/2/8.
+ */
+public class ImagePageProcesser implements PageProcessor {
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
