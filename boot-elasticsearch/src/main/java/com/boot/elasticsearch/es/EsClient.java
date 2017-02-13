423127834c4fb47524b31d46fafd14d93f171e01
ZMMWMY <13758212259@163.com>
2017-02-13 16:52:14 +0800

    es 搜索

--- boot-elasticsearch/src/main/java/com/boot/elasticsearch/es/EsClient.java
+++ boot-elasticsearch/src/main/java/com/boot/elasticsearch/es/EsClient.java
@@ -1,10 +1,14 @@
 package com.boot.elasticsearch.es;
 
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
@@ -38,11 +42,39 @@ public class EsClient {
         System.out.println("索引Id:" + response.getId());
     }
 
+    public static void searchByFields(String ... strings){
+        Client client =getTransport();
+        SearchResponse response = client.prepareSearch("sina").setQuery(QueryBuilders.queryStringQuery(strings[0])).execute().actionGet();
+        SearchHits searchHits = response.getHits();
+        System.out.println("共检索到"+searchHits.getTotalHits()+"条记录");
+        SearchHit [] hits = searchHits.getHits() ;
+        for (SearchHit hit: hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+    }
+
+    public static void searchIndex(){
+        Client client =getTransport();
+        SearchResponse response = client.prepareSearch("sina").execute().actionGet();
+        SearchHits searchHits = response.getHits();
+        System.out.println("共检索到"+searchHits.getTotalHits()+"条记录");
+        SearchHit [] hits = searchHits.getHits() ;
+        for (SearchHit hit: hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+    }
+
+
     public static void test(){
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("test","test");
         IndexResponse response = getTransport().prepareIndex("test","test").setSource(map).execute().actionGet();
         System.out.println(response.getId());
 
+    }
+
+    public static void main(String[] args) {
+        searchByFields("第一");
+   //     searchIndex();
     }
 }
