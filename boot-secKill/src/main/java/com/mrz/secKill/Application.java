5c0af664bfef9afc4d012e9abd5a3d223061746d
ZMMWMY <13758212259@163.com>
2017-05-18 17:50:53 +0800

    secKill 启动类

---
+++ boot-secKill/src/main/java/com/mrz/secKill/Application.java
@@ -0,0 +1,17 @@
+package com.mrz.secKill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author : MrZ
+ *
+ * @Description Created in 17:48 on 2017/5/18.
+ * Modified By :
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
