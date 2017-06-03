914a5223bd675ebc1adac5ea8b2483f93082cc7d
ZMM <13758212259@163.com>
2017-06-03 16:16:58 +0800

    初始jquery

--- boot-security/src/main/java/com/security/controller/IndexController.java
+++ boot-security/src/main/java/com/security/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @RequestMapping(value = {"", "/home"}, method = RequestMethod.GET)
+    @ResponseBody
     public String home() {
         return "home";
     }
--- boot-security/src/main/resources/新建文件夹/login.js
+++ boot-security/src/main/resources/新建文件夹/login.js
@@ -23,3 +23,23 @@
                   })
         })
     }
+
+    function data(){
+      $.ajax({
+        url: 'http://localhost:8081',
+        type: 'get',
+        // dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+        // data: {param1: 'value1'},
+      })
+      .done(function(data) {
+        console.log("success");
+        $("#city").text(data);
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+      
+    }
